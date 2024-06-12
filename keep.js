eedcaec3784d03b5c7b67f1b1bb8852e5fa72460
shaozeye <shaozeye@outlook.com>
2024-06-12 16:52:48 +0800

    keep

---
+++ keep.js
@@ -0,0 +1,26 @@
+/*******************************
+
+    [rewrite_local]
+
+    ^ https: \/\/api\.gotokeep\.com url script-response-body https:/ / raw.githubusercontent.com / 89996462 / Quantumult - X / main / ycdz / keep.js
+
+    [mitm]
+
+hostname = api.gotokeep.com
+
+*******************************/
+
+var modifiedBody = $response.body
+    .replace(/\"memberStatus":\d+/g, '"memberStatus":1')
+    .replace(/\"username":".*?"/g, '"username":"PremiumUser"')
+    .replace(/\"buttonText":".*?"/g, '"buttonText":"Unlocked"')
+    .replace(/\"hasPaid\":\w+/g, '"hasPaid":true')
+    .replace(/\"downLoadAll\":\w+/g, '"downLoadAll":true')
+    .replace(/\"videoTime\":\d+/g, '"videoTime":3000')
+    .replace(/\"startEnable\":\w+/g, '"startEnable":true')
+    .replace(/\"memberStatus\":\d+/g, '"memberStatus":1')
+    .replace(/\"preview\":\w+/g, '"preview":true')
+    .replace(/\"errorCode\":\d+/g, '"errorCode":0')
+    .replace(/\"status\":\w+/g, '"status":true');
+
+$done({ body: modifiedBody });
