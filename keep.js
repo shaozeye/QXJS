73874303308bf0e00611b8c1e75e3fc01a3e1dae
shaozeye <36854025+shaozeye@users.noreply.github.com>
2024-07-19 13:42:08 +0800

    Update keep.js

--- keep.js
+++ keep.js
@@ -10,17 +10,5 @@ hostname = api.gotokeep.com
 
 */
 
-var modifiedBody = $response.body
-    .replace(/\"memberStatus":\d+/g, '"memberStatus":1')
-    .replace(/\"username":".*?"/g, '"username":"PremiumUser"')
-    .replace(/\"buttonText":".*?"/g, '"buttonText":"Unlocked"')
-    .replace(/\"hasPaid\":\w+/g, '"hasPaid":true')
-    .replace(/\"downLoadAll\":\w+/g, '"downLoadAll":true')
-    .replace(/\"videoTime\":\d+/g, '"videoTime":3000')
-    .replace(/\"startEnable\":\w+/g, '"startEnable":true')
-    .replace(/\"memberStatus\":\d+/g, '"memberStatus":1')
-    .replace(/\"preview\":\w+/g, '"preview":true')
-    .replace(/\"errorCode\":\d+/g, '"errorCode":0')
-    .replace(/\"status\":\w+/g, '"status":true');
-
+var modifiedBody = $response.body.replace(/\"memberStatus":\d+/g, '"memberStatus":1').replace(/\"username":".*?"/g, '"username":"PremiumUser"').replace(/\"buttonText":".*?"/g, '"buttonText":"Unlocked"').replace(/\"hasPaid\":\w+/g, '"hasPaid":true').replace(/\"downLoadAll\":\w+/g, '"downLoadAll":true').replace(/\"videoTime\":\d+/g, '"videoTime":3000').replace(/\"startEnable\":\w+/g, '"startEnable":true').replace(/\"memberStatus\":\d+/g, '"memberStatus":1').replace(/\"preview\":\w+/g, '"preview":true').replace(/\"errorCode\":\d+/g, '"errorCode":0').replace(/\"status\":\w+/g, '"status":true');
 $done({ body: modifiedBody });
