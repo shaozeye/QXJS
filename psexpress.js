006838352521db3f5ab03f451f4870a92d65a684
shaozeye <36854025+shaozeye@users.noreply.github.com>
2024-09-06 03:11:11 +0800

    更新 psexpress.js

--- psexpress.js
+++ psexpress.js
@@ -4,7 +4,7 @@ PSExpress 23.49.1
 2023.12.15
 [rewrite_local]
 
-https://lcs-mobile-cops.adobe.io/mobiles/access_profile/v3 url script-response-body https://raw.githubusercontent.com/shaozeye/QXJS/main/ths.js
+https://lcs-mobile-cops.adobe.io/mobiles/access_profile/v3 url script-response-body https://raw.githubusercontent.com/shaozeye/QXJS/main/psexpress.js
 
 [mitm] 
 
